54486cca506eb02f7267aec709727653822bbd3c
Satyam Thakur <contact.satyamthakur@gmail.com>
2025-06-23 15:36:36 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,266 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Innovator", imgPath: "/images/ideas.svg" },
+  { text: "Engineer", imgPath: "/images/concepts.svg" },
+  { text: "Speaker", imgPath: "/images/designs.svg" },
+  { text: "Developer", imgPath: "/images/code.svg" },
+  { text: "Innovator", imgPath: "/images/ideas.svg" },
+  { text: "Engineer", imgPath: "/images/concepts.svg" },
+  { text: "Speaker", imgPath: "/images/designs.svg" },
+  { text: "Developer", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 3, suffix: "+", label: "Years of Tech Experience" },
+  { value: 8, suffix: "+", label: "Real-world Projects Delivered" },
+  { value: 4, suffix: "+", label: "ndustry Certifications" },
+  { value: 3, suffix: "", label: "Keynote Appearances" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Precision & Quality",
+    desc: "Every project I take on is treated with engineering precision. From writing scalable code to ensuring pixel-perfect UI, I deliver high-quality solutions that not only work but delight users.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Clear, Consistent Communication",
+    desc: "Whether we’re sprint planning or iterating on feedback, I keep you in the loop. Expect timely updates, transparent progress, and a collaborative workflow that aligns with your vision.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Having led multiple time-sensitive projects—from machine learning models to live event apps—I understand the value of your time. I ensure on-schedule delivery without compromising quality.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+const expCards = [
+  {
+    review: "Satyam brought precision and structure to our QA workflows. His testing automation significantly improved our product reliability and ensured quicker release cycles.",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    title: "Software Test Engineer",
+    date: "November 2024 - December 2024",
+    responsibilities: [
+      "Designed and executed automated and manual test cases using Selenium, Postman, and JIRA across regression, functional, and integration suites.",
+      "Conducted complete bug lifecycle management, identifying and resolving UI inconsistencies, memory leaks, and API failures.",
+      "Built modular test environments, boosting overall test coverage and execution efficiency.",
+    ],
+  },
+  {
+    review: "Satyam's data-driven environmental tech initiatives showcased his leadership, technical expertise, and ability to integrate scalable software practices into impact-focused domains.",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/logo2.png",
+    title: "Data Analyst & Project Manager",
+    date: "May 2022 - April 2024",
+    responsibilities: [
+      "Engineered Python-based analytics pipelines for 5 environmental programs, improving decision-making with actionable data insights.",
+      "Applied Agile methodologies to manage and deliver 3 tech-driven sustainability projects (waste optimization, renewable energy apps, climate literacy portals)",
+      "Used SEO tools and digital platforms to run public engagement campaigns, applying web analytics to optimize reach and adoption.",
+    ],
+  },
+  {
+    review: "Satyam’s cross-functional skills in marketing tech and design translated into seamless event platforms and an elevated user experience for the Under 25 Summit.",
+    imgPath: "/images/exp3.png",
+    logoPath: "/images/logo3.png",
+    title: "Event Marketing & Production Intern",
+    date: "February 2023 - March 2023",
+    responsibilities: [
+      "Collaborated with tech and design teams to build audience-facing campaign pages using Adobe tools and marketing automation workflows.",
+      "Boosted campaign conversion by 20% via SEO & PPC integrations; applied analytics to refine messaging and improve ROI.",
+      "Managed digital assets and process flows using tools like Trello and Asana to optimize cross-functional event execution.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Product Manager",
+    mentions: "Wyreflow",
+    review: "Satyam quickly grasped our complex testing pipeline and brought structure and clarity to our QA processes. His ability to detect edge-case failures and optimize testing workflows made a huge difference",
+  },
+  {
+    name: "Program Director",
+    mentions: "Restless Development",
+    review: "Satyam’s tech-driven approach to climate advocacy is unmatched. From handling complex data analytics to delivering community-based solutions, he led initiatives like a seasoned product owner.",
+  },
+  {
+    name: "Event Head",
+    mentions: "Under 25 Summit 2023",
+    review: "Satyam brought a unique tech-first mindset to event production. His knack for digital storytelling and automation helped scale our marketing efforts like never before",
+  },
+  {
+    name: "Technische TechExpo",
+    mentions: "IIT Guwahati",
+    review: "Satyam’s keynote at our TechExpo was both visionary and grounded. His demo of applying tech to solve real-world problems inspired a packed hall of budding engineers and entrepreneurs.",
+  },
+  {
+    name: "Program Manager",
+    mentions: "Saahas",
+    review: "Satyam transformed our awareness campaign into a tech-enabled experience. His use of visual storytelling and flow design made sustainability education engaging and measurable",
+  },
+  {
+    name: "UL Innovation Panel Judge",
+    mentions: "NSSQ Event",
+    review: "At just 17, Satyam had a room full of professionals on the edge of their seats. His talk on breaking boundaries with technology reminded us why investing in youth innovation matters.",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
