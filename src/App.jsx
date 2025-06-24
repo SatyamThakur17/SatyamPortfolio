9bf8c8954051f99e9f34018ea3f96e6c1fec2f2b
Satyam Thakur <contact.satyamthakur@gmail.com>
2025-06-24 15:17:54 +0530

    fix: corrected ShowcaseSection import path

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Contact from "./sections/Contact";
 import TechStack from "./sections/TechStack";
 import Experience from "./sections/Experience";
 import Hero from "./sections/Hero";
-import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import LogoShowcase from "./sections/LogoShowcase";
+import ShowcaseSection from "./sections/ShowcaseSection";
 import FeatureCards from "./sections/FeatureCards";
 import Navbar from "./components/NavBar";
 
